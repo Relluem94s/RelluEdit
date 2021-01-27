b50f073771ed0c39c40daf9df92b5de6cb110446
Relluem94 <github@relluem94.de>
2021-01-27 01:42:57 +0100

    renamed a bunch of variables

--- src/de/relluem94/relluedit/functions/Frames.java
+++ src/de/relluem94/relluedit/functions/Frames.java
@@ -16,7 +16,7 @@ public class Frames extends Variables {
     public JInternalFrame consoleFrame = ConsoleFrame();
     public JInternalFrame Relluem94Frame = Relluem94Frame();
     public JInternalFrame VersionFrame = VersionFrame();
-    public static JInternalFrame WierdThingFrame = WierdThingFrame();
+    public static JInternalFrame WierdThingFrame = WeirdThingFrame();
     public JInternalFrame preferencesFrame = PreferencesFrame();
 
     public void addFrameToList(JInternalFrame frame) {
@@ -37,8 +37,8 @@ public class Frames extends Variables {
 
     public JInternalFrame ReplaceLineFrame() {
 
-        String title = bundle.getString("l_replaceline");
-        Dimension size = new Dimension(450, 250);
+        String replaceLineFrameTitle = bundle.getString("l_replaceline");
+        Dimension replaceLineFrameSize = new Dimension(450, 250);
 
         boolean iconifiable = true;
         boolean resizable = false;
@@ -46,7 +46,7 @@ public class Frames extends Variables {
         boolean dispose = true;
         boolean snapper = true;
 
-        InternalFrames iframe = new InternalFrames(title, size, size, size, size, iconifiable, resizable, closable, dispose, snapper);
+        InternalFrames iframe = new InternalFrames(replaceLineFrameTitle, replaceLineFrameSize, replaceLineFrameSize, replaceLineFrameSize, replaceLineFrameSize, iconifiable, resizable, closable, dispose, snapper);
         Panes pane = new Panes(this, iframe);
 
         return iframe.makeFrame(pane.getReplaceLine());
@@ -54,8 +54,8 @@ public class Frames extends Variables {
 
     public JInternalFrame PreferencesFrame() {
 
-        String title = bundle.getString("l_preferences");
-        Dimension size = new Dimension(450, 190);
+        String preferencesFrameTitle = bundle.getString("l_preferences");
+        Dimension preferencesFrameSize = new Dimension(450, 190);
 
         boolean iconifiable = false;
         boolean resizable = false;
@@ -63,7 +63,7 @@ public class Frames extends Variables {
         boolean dispose = true;
         boolean snapper = true;
 
-        InternalFrames iframe = new InternalFrames(title, size, size, size, size, iconifiable, resizable, closable, dispose, snapper);
+        InternalFrames iframe = new InternalFrames(preferencesFrameTitle, preferencesFrameSize, preferencesFrameSize, preferencesFrameSize, preferencesFrameSize, iconifiable, resizable, closable, dispose, snapper);
         Panes pane = new Panes(this, iframe);
 
         return iframe.makeFrame(pane.getPreferences());
@@ -71,8 +71,8 @@ public class Frames extends Variables {
 
     public JInternalFrame FindFrame() {
 
-        String title = bundle.getString("l_find");
-        Dimension size = new Dimension(450, 190);
+        String findFrameTitle = bundle.getString("l_find");
+        Dimension findFrameSize = new Dimension(450, 190);
 
         boolean iconifiable = true;
         boolean resizable = false;
@@ -80,7 +80,7 @@ public class Frames extends Variables {
         boolean dispose = true;
         boolean snapper = true;
 
-        InternalFrames iframe = new InternalFrames(title, size, size, size, size, iconifiable, resizable, closable, dispose, snapper);
+        InternalFrames iframe = new InternalFrames(findFrameTitle, findFrameSize, findFrameSize, findFrameSize, findFrameSize, iconifiable, resizable, closable, dispose, snapper);
         Panes pane = new Panes(this, iframe);
 
         return iframe.makeFrame(pane.getFind());
@@ -88,8 +88,8 @@ public class Frames extends Variables {
 
     public JInternalFrame ReplaceFrame() {
 
-        String title = bundle.getString("l_replace");
-        Dimension size = new Dimension(450, 190);
+        String replaceFrameTitle = bundle.getString("l_replace");
+        Dimension replaceFrameSize = new Dimension(450, 190);
 
         boolean iconifiable = true;
         boolean resizable = false;
@@ -97,16 +97,16 @@ public class Frames extends Variables {
         boolean dispose = true;
         boolean snapper = true;
 
-        InternalFrames iframe = new InternalFrames(title, size, size, size, size, iconifiable, resizable, closable, dispose, snapper);
+        InternalFrames iframe = new InternalFrames(replaceFrameTitle, replaceFrameSize, replaceFrameSize, replaceFrameSize, replaceFrameSize, iconifiable, resizable, closable, dispose, snapper);
         Panes pane = new Panes(this, iframe);
 
         return iframe.makeFrame(pane.getReplace());
     }
 
-    public static JInternalFrame WierdThingFrame() {
+    public static JInternalFrame WeirdThingFrame() {
 
-        String title = bundle.getString("l_relluem94");
-        Dimension size = new Dimension(501, 504);
+        String weirdThingFrameTitle = bundle.getString("l_relluem94");
+        Dimension weirdThingFrameSize = new Dimension(501, 504);
 
         boolean iconifiable = false;
         boolean resizable = false;
@@ -114,15 +114,15 @@ public class Frames extends Variables {
         boolean dispose = true;
         boolean snapper = true;
 
-        InternalFrames iframe = new InternalFrames(title, size, size, size, size, iconifiable, resizable, closable, dispose, snapper);
+        InternalFrames iframe = new InternalFrames(weirdThingFrameTitle, weirdThingFrameSize, weirdThingFrameSize, weirdThingFrameSize, weirdThingFrameSize, iconifiable, resizable, closable, dispose, snapper);
 
         return iframe.makeFrame(new CubePanel());
     }
 
     public JInternalFrame VersionFrame() {
 
-        String title = bundle.getString("l_version");
-        Dimension size = new Dimension(450, 290);
+        String versionFrameTitle = bundle.getString("l_version");
+        Dimension versionFrameSize = new Dimension(450, 290);
 
         boolean iconifiable = false;
         boolean resizable = false;
@@ -130,7 +130,7 @@ public class Frames extends Variables {
         boolean dispose = true;
         boolean snapper = true;
 
-        InternalFrames iframe = new InternalFrames(title, size, size, size, size, iconifiable, resizable, closable, dispose, snapper);
+        InternalFrames iframe = new InternalFrames(versionFrameTitle, versionFrameSize, versionFrameSize, versionFrameSize, versionFrameSize, iconifiable, resizable, closable, dispose, snapper);
         Panes pane = new Panes(this, iframe);
 
         return iframe.makeFrame(pane.getVersion());
@@ -138,8 +138,8 @@ public class Frames extends Variables {
 
     public JInternalFrame Relluem94Frame() {
 
-        String title = bundle.getString("l_relluem94");
-        Dimension size = new Dimension(450, 190);
+        String relluem94FrameTitle = bundle.getString("l_relluem94");
+        Dimension relluem94FrameSize = new Dimension(450, 190);
 
         boolean iconifiable = false;
         boolean resizable = false;
@@ -147,7 +147,7 @@ public class Frames extends Variables {
         boolean dispose = true;
         boolean snapper = true;
 
-        InternalFrames iframe = new InternalFrames(title, size, size, size, size, iconifiable, resizable, closable, dispose, snapper);
+        InternalFrames iframe = new InternalFrames(relluem94FrameTitle, relluem94FrameSize, relluem94FrameSize, relluem94FrameSize, relluem94FrameSize, iconifiable, resizable, closable, dispose, snapper);
         Panes pane = new Panes(this, iframe);
 
         return iframe.makeFrame(pane.getRelluem94());
@@ -155,11 +155,11 @@ public class Frames extends Variables {
 
     public JInternalFrame EditorFrame() {
 
-        String title = bundle.getString("l_editor");
+        String editorFrameTitle = bundle.getString("l_editor");
         Dimension maxSize = new Dimension(screenSize.width, screenSize.height - 135);
         Dimension minSize = new Dimension(360, 460);
         Dimension prefSize = new Dimension(WIDTH - 500, HEIGHT - 200);
-        Dimension size = new Dimension(WIDTH - 500, HEIGHT - 200);
+        Dimension editorFrameSize = new Dimension(WIDTH - 500, HEIGHT - 200);
 
         boolean iconifiable = true;
         boolean resizable = true;
@@ -167,7 +167,7 @@ public class Frames extends Variables {
         boolean dispose = false;
         boolean snapper = true;
 
-        InternalFrames iframe = new InternalFrames(title, minSize, maxSize, prefSize, size, iconifiable, resizable, closable, dispose, snapper);
+        InternalFrames iframe = new InternalFrames(editorFrameTitle, minSize, maxSize, prefSize, editorFrameSize, iconifiable, resizable, closable, dispose, snapper);
         Panes pane = new Panes(this, iframe);
 
         return iframe.makeFrame(pane.getEditor());
@@ -175,8 +175,8 @@ public class Frames extends Variables {
 
     public JInternalFrame ConsoleFrame() {
 
-        String title = bundle.getString("l_console");
-        Dimension size = new Dimension(screenSize.width / 2, screenSize.height / 2);
+        String consoleFrameTitle = bundle.getString("l_console");
+        Dimension consoleFrameSize = new Dimension(screenSize.width / 2, screenSize.height / 2);
 
         boolean iconifiable = true;
         boolean resizable = false;
@@ -184,7 +184,7 @@ public class Frames extends Variables {
         boolean dispose = true;
         boolean snapper = true;
 
-        InternalFrames iframe = new InternalFrames(title, size, size, size, size, iconifiable, resizable, closable, dispose, snapper);
+        InternalFrames iframe = new InternalFrames(consoleFrameTitle, consoleFrameSize, consoleFrameSize, consoleFrameSize, consoleFrameSize, iconifiable, resizable, closable, dispose, snapper);
         Panes pane = new Panes(this, iframe);
 
         return iframe.makeFrame(pane.getConsole());
