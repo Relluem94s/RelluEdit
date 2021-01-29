390ad17730c9d92295a45d2a857ff41ef07de830
Relluem94 <github@relluem94.de>
2021-01-30 00:24:35 +0100

    format

--- src/de/relluem94/relluedit/api/toolbox/Toolbox.java
+++ src/de/relluem94/relluedit/api/toolbox/Toolbox.java
@@ -1,6 +1,7 @@
 package de.relluem94.relluedit.api.toolbox;
 
 public class Toolbox extends Menus {
+
     public void setText(String text) {
         content = text;
         textPane.setText(content);
