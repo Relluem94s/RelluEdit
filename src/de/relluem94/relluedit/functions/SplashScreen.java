36b676915c2682543440641aeb077444f1e855e1
Relluem94 <github@relluem94.de>
2021-01-30 00:25:00 +0100

    format

--- src/de/relluem94/relluedit/functions/SplashScreen.java
+++ src/de/relluem94/relluedit/functions/SplashScreen.java
@@ -31,7 +31,7 @@ public class SplashScreen extends JFrame {
         setLocationRelativeTo(null);
         setUndecorated(true);
         img = images.getImage("logo_relluedit.png");
-        Image newimg = img.getScaledInstance(518/2, 183/2, java.awt.Image.SCALE_SMOOTH);
+        Image newimg = img.getScaledInstance(518 / 2, 183 / 2, java.awt.Image.SCALE_SMOOTH);
         getContentPane().setBackground(new Color(10, 10, 10, 0));
         setLayout(null);
         pbar = new JProgressBar();
